f9180d969ace18e6fcddc3878e87e8ba0f61079d
dalerfay <88854865+dalerfay@users.noreply.github.com>
2022-03-17 21:05:27 +0300

    new site

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+ $('.section-1-slider,.section-2-slider').slick({
+    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="./images/back left.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="./images/next right.svg" alt=""></button>',
+    autoplay: true,
+    fade:true
+  });
+ 
+
+
+
+});
